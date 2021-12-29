ec1b23b5de715e40e3fffdb8cfa01313de25f355
Manuel <manuelcu2299@hotmail.com>
2021-12-29 09:52:06 -0600

    se empezo a trabajar en el componente de checkout

---
+++ src/Checkout.js
@@ -0,0 +1,26 @@
+import React from "react";
+import banner from "./images/banner.jpg";
+
+function Checkout() {
+  return (
+    <div className="checkout flex p-[20px] bg-white h-max">
+      <div className="checkout_left">
+        <img
+          className="checkout_ad w-full mb-[10px] h-[150px] object-cover"
+          src={banner}
+          alt=""
+        />
+        <div>
+          <h2 className="checkout_title mr-[10px] p-[10px] border-b border-gray-300">
+            Tu carrito de compras
+          </h2>
+        </div>
+      </div>
+      <div className="checkout_right">
+        <h2>The subtotal will go here</h2>
+      </div>
+    </div>
+  );
+}
+
+export default Checkout;
