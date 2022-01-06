6ac644acf42e2ec389ca3819473eee3cdad80810
Manuel <manuelcu2299@hotmail.com>
2022-01-06 10:25:32 -0600

    Se hicieron cambios en la imagen de la pagina de checkout

--- src/Checkout.js
+++ src/Checkout.js
@@ -5,14 +5,14 @@ import Subtotal from "./Subtotal";
 function Checkout() {
   return (
     <div className="checkout flex p-[20px] bg-white h-max">
-      <div className="checkout_left">
+      <div className="checkout_left mx-[5px]">
         <img
-          className="checkout_ad w-full mb-[10px] h-[150px] object-cover"
-          src={banner}
+          className="checkout_ad w-full mb-[10px]"
+          src="https://images-na.ssl-images-amazon.com/images/G/02/UK_CCMP/TM/OCC_Amazon1._CB423492668_.jpg"
           alt=""
         />
         <div>
-          <h2 className="checkout_title mr-[10px] p-[10px] border-b border-gray-300">
+          <h2 className="checkout_title mr-[10px] p-[10px] border-b border-gray-300 font-bold">
             Tu carrito de compras
           </h2>
         </div>
